2f0a163ae4a136cc28211f045f70861e47aee9af
Raahul M <bharath.raahul-m@capgemini.com>
2024-02-23 11:15:07 +0530

    Final Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ColorPicker from './ColorPicker'
+
+function App() {
+
+  return(
+    <ColorPicker></ColorPicker>
+  )
+  
+}
+
+export default App
